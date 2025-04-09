7067ed951657a262295af27991f01ce5b1e3b4b6
Joshua Daniels-Kemp <44926846+WitheredHope@users.noreply.github.com>
2025-04-09 11:29:54 +0100

    fixed reference error

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { supabase } from '../services/supabase';
 import { useNavigate, Link, useSearchParams } from 'react-router-dom';
 import styles from './Login.module.css';
